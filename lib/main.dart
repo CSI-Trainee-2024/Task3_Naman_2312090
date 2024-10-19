a5326bf152fa587d4d83f2af6848507e443901ad
Naman2004singh <naman.2singh9@gmail.com>
2024-10-20 00:21:51 +0530

    created the appScreen

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import '../../i/lib/screens/Home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
